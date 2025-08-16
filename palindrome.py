a5e0811b87eb79473f92c5200432dc304d7fc279
khadija-codencoit <khadija.khatun@codencoit.com>
2025-08-16 21:45:02 +0600

    Solving Palindrome Number Using a Class

--- palindrome.py
+++ palindrome.py
@@ -1,10 +1,20 @@
-value = input("Enter the value : ")
+# value = input("Enter the value : ")
 
-reverse = value[::-1]
+# reverse = value[::-1]
 
-if value == reverse:
-    print("Yes! this is palindrome")
-else:
-    print("This is not palindrome")
+# if value == reverse:
+#     print("Yes! this is palindrome")
+# else:
+#     print("This is not palindrome")
+
+class Solution:
+    def isPalindrome(self,x):
+        if x < 0:
+            return False
+        str_x = str(x)
+        return str_x == str_x[::-1]
 
 
+solution = Solution()
+
+print(solution.isPalindrome(121)) 
